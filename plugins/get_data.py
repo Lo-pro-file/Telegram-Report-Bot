bac6b5f267463dfe9a62072ef90ab5fff40c60a3
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-16 10:51:25 +0530

    Update get_data.py

--- plugins/get_data.py
+++ plugins/get_data.py
@@ -5,15 +5,7 @@
 
 # #workdir = 'session/'disconect
 # async def main():
-#      config = (json.load(open("config.json")))
-#      for account in config["accounts"]:
-#         phone = account ["phone"]
-#         async with Client(phone, workdir="session") as app:
-#             print( phone, "is logined") if await app.get_me() else print(phone, "login failed")
-#             async for dialog in app.get_dialogs():
-#                 print(dialog.chat.first_name or dialog.chat.title)
-            
-            
+            # function not available yet
 
             
 # asyncio.run(main())
