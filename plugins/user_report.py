898d91e293d52abfab2c30b19a88f75fe550c139
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-16 12:30:18 +0530

    Update user_report.py

--- plugins/user_report.py
+++ plugins/user_report.py
@@ -7,15 +7,18 @@ from pyrogram.types import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from info import Config, Txt
 
 
-config_path = Path("config.json") 
+config_path = Path("config.json")
+
 
 async def Report_Function(No, msg):
-    
-    listofchoise = ['Report for child abuse', 'Report for copyrighted content', 'Report for impersonation', 'Report an irrelevant geogroup', 'Report an illegal durg','Report for Violence', 'Report for offensive person detail', 'Reason for Pornography', 'Report for spam"']
+
+    listofchoise = ['Report for child abuse', 'Report for copyrighted content', 'Report for impersonation', 'Report an irrelevant geogroup',
+                    'Report an illegal durg', 'Report for Violence', 'Report for offensive person detail', 'Reason for Pornography', 'Report for spam"']
     message = listofchoise[int(No) - 1]
 
     # Run a shell command and capture its output
-    result = subprocess.run(["python", "report.py", f"{message}"], shell=True, capture_output=True, text=True)
+    result = subprocess.run(
+        ["python", "report.py", f"{message}"], shell=True, capture_output=True, text=True)
     print("Reuslt", result)
 
     # Check the return code to see if the command was successful
@@ -24,7 +27,7 @@ async def Report_Function(No, msg):
         print("Command output:")
         print(result.stdout)
         return [result.stdout, True]
-        
+
     else:
         # Print the error message if the command failed
         print("Command failed with error:")
@@ -33,10 +36,10 @@ async def Report_Function(No, msg):
 
 
 async def CHOICE_OPTION(bot, msg, number):
-    
+
     if not config_path.exists():
-            return await msg.reply_text(text="**You don't have any config first make the config then you'll able to report**\n\n Use /make_config", reply_to_message_id=msg.id)
-        
+        return await msg.reply_text(text="**You don't have any config first make the config then you'll able to report**\n\n Use /make_config", reply_to_message_id=msg.id, reply_markup=ReplyKeyboardRemove())
+
     with open(config_path, 'r', encoding='utf-8') as file:
         config = json.load(file)
 
@@ -61,60 +64,57 @@ async def CHOICE_OPTION(bot, msg, number):
         await bot.send_message(chat_id=msg.chat.id, text=f"{result}", reply_to_message_id=msg.id)
 
 
-
 @Client.on_message(filters.private & filters.user(Config.OWNER) & filters.command('report'))
-async def handle_report(bot:Client, cmd:Message):
-    
+async def handle_report(bot: Client, cmd: Message):
+
     CHOICE = [
-        [("1"),("2")], [("3"), ("4")], [("5"), ("6")], [("7"), ("8")], [("9")]
+        [("1"), ("2")], [("3"), ("4")], [("5"), ("6")], [("7"), ("8")], [("9")]
     ]
 
     await bot.send_message(chat_id=cmd.from_user.id, text=Txt.REPORT_CHOICE, reply_to_message_id=cmd.id, reply_markup=ReplyKeyboardMarkup(CHOICE, resize_keyboard=True))
 
 
 @Client.on_message(filters.regex("1"))
-async def one(bot:Client, msg:Message):
+async def one(bot: Client, msg: Message):
 
     await CHOICE_OPTION(bot, msg, 1)
 
 
-
 @Client.on_message(filters.regex("2"))
-async def two(bot:Client, msg:Message):
+async def two(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 2)
 
 
 @Client.on_message(filters.regex("3"))
-async def three(bot:Client, msg:Message):
+async def three(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 3)
 
 
 @Client.on_message(filters.regex("4"))
-async def four(bot:Client, msg:Message):
+async def four(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 4)
 
 
 @Client.on_message(filters.regex("5"))
-async def five(bot:Client, msg:Message):
+async def five(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 5)
 
 
 @Client.on_message(filters.regex("6"))
-async def six(bot:Client, msg:Message):
+async def six(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 6)
 
 
 @Client.on_message(filters.regex("7"))
-async def seven(bot:Client, msg:Message):
+async def seven(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 7)
 
 
 @Client.on_message(filters.regex("8"))
-async def eight(bot:Client, msg:Message):
+async def eight(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 8)
 
 
 @Client.on_message(filters.regex("9"))
-async def nine(bot:Client, msg:Message):
+async def nine(bot: Client, msg: Message):
     await CHOICE_OPTION(bot, msg, 9)
-
